0e51afe65566cfbbaa871e78665bb6c07d237f39
tafsirul-islam <tafsirulislam093@gmail.com>
2025-05-09 14:15:28 +0600

    change all array

--- array.c
+++ array.c
@@ -1,7 +1,21 @@
-//C programme #include<stdio.h>
+#include<stdio.h>
 
-int main() { // Array int rows,cols; printf("Enter row and column :\n"); printf("row = "); scanf("%d",&rows); printf("columns = "); scanf("%d",&cols); int matrix[rows][cols];
-
+int main()
+{ 
+    // Array
+    int rows,cols;
+    printf("Enter row and column :\n");
+    printf("row = ");
+    scanf("%d",&rows);
+    printf("columns = ");
+    scanf("%d",&cols);
+    
+    if(rows!=cols){
+    printf("It's not possible.");
+    }
+    
+    else{
+    int matrix[rows][cols],c=0;
     printf("Enter your matrix value:\n");
     for( int row=0;row<rows;row++){
       for(int col=0;col<cols;col++){
@@ -12,7 +26,7 @@ int main() { // Array int rows,cols; printf("Enter row and column :\n"); printf(
     }
     
     
-    printf("Matrix is = \n");
+    printf("\nMatrix is = \n");
      for(int row=0;row<rows;row++){
       for(int col=0;col<cols;col++){
         printf("%d ",matrix[row][col]);
@@ -20,7 +34,8 @@ int main() { // Array int rows,cols; printf("Enter row and column :\n"); printf(
       printf("\n");
     }
     
-    printf("T Matrix is = \n");
+    //Transpose matrix
+    printf("\nT Matrix is = \n");
      for(int row=0;row<rows;row++){
        for(int col=0;col<cols;col++){
          printf("%d ",matrix[col][row]);
@@ -28,5 +43,48 @@ int main() { // Array int rows,cols; printf("Enter row and column :\n"); printf(
       printf("\n");
     }
     
-    return 0;
+    //symmetry matrix
+    for(int row=0;row<rows;row++){
+     for(int col=0;col<cols;col++){
+      if(matrix[row][col]!=matrix[col][row]){
+      c=c+1;
+      }
+     }
     }
+    if(c==0)
+    printf("\nIt is symmetry matrix.");
+    else
+    printf("\nIt's not symmetry matrix.");
+    
+    //diagonal elements
+    int sum=0;
+    printf("\n\nDiagonal elements is\n"); 
+    for(int row=0;row<rows;row++){
+     for(int col=0;col<cols;col++){
+      if(col==row){
+        printf("%d ",matrix[row][col]);
+        sum+=matrix[row][col];
+      }
+     }
+    }
+    printf("\nSum of main diagonal elements =%d",sum);
+    
+    
+    int sum1=0;
+    printf("\n\nNondiagonal elements is\n"); 
+    for(int row=0;row<rows;row++){
+     for(int col=0;col<cols;col++){
+      if(col!=row){
+        printf("%d ",matrix[row][col]);
+        sum1+=matrix[row][col];
+      }
+     }
+    }
+    printf("\nSum of non-diagonal elements =%d",sum1);
+    
+    
+    
+   } 
+   
+    return 0;
+}
